599cb83c10516d4a6554852e60c69eacf1cd7824
Paul Jakob Kroker <pauljakobkroker@gmail.com>
2025-09-16 18:23:26 +0200

    added alpha codium

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+from study_llm_codegeneration.codegen_09_alpha_codium.main import main as ac
 from study_llm_codegeneration.codegen_10_open_code_interpreter.main import main as chatbot
+
 
 def main():
     print("helloworld")
-    chatbot()
+    ac()
+    #chatbot()
 
 if __name__ == "__main__":
     main()
